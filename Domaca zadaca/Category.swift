13945f9b97451a15790ec7c8bb0426f08b1a2499
marinsimic98 <48602216+marinsimic98@users.noreply.github.com>
2019-04-07 20:51:32 +0200

    Add files via upload

---
+++ Domaca zadaca/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  Domaca zadaca
+//
+//  Created by Marin Šimić on 06/04/2019.
+//  Copyright © 2019 Marin Šimić. All rights reserved.
+//
+
+import Foundation
+
+enum Category{
+    case sport
+    case science
+}
